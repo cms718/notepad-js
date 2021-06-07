2a1f22a2b304872bad45bae2cfa2014ed101b78b
BecaLParker <becaparker@gmail.com>
2021-06-07 15:55:08 +0100

    displaying empty list

--- src/notepad.js
+++ src/notepad.js
@@ -1,3 +1,5 @@
 class Notepad {
-
+  list() {
+    return []
+  }
 }
--- tests/notepad-tests.js
+++ tests/notepad-tests.js
@@ -1,8 +1,21 @@
 'strict';
 
-function testDisplayList() {
+function testDisplayEmptyList() {
   var notepad = new Notepad();
-  console.log(notepad)
+  console.log('Starts empty?')
+  if(notepad.list().length === 0) {
+    console.log('PASS')
+  } else console.log('FAIL')
 };
 
-testDisplayList();
+function testDisplayListOfNotes() {
+  var notepad = new Notepad();
+  console.log('Displays list?')
+  notepad.notes.push('first note')
+  if(notepad.list()[0] === 'first note') {
+    console.log('PASS')
+  } else console.log('FAIL')
+};
+
+testDisplayEmptyList();
+testDisplayListOfNotes();
