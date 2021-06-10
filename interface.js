b5237890b8516d5263f7c89d42c142d8cda18277
Charlie Slater <charliemslater1701@gmail.com>
2021-06-10 14:47:12 +0100

    occupy list of notes

--- interface.js
+++ interface.js
@@ -4,5 +4,18 @@ document.addEventListener("DOMContentLoaded", () => {
   document.getElementById("add-note").addEventListener("click", () => {
     let noteInputValue = document.getElementById("note-input").value;
     notepad.addNote(noteInputValue);
+    getNotes();
   });
+
+  getNotes = () => {
+    let notes = notepad.shortenedNotes();
+    let ul = document.getElementById("list");
+    ul.innerHTML = "";
+    notes.forEach((note) => {
+      let li = document.createElement("li");
+      li.appendChild(document.createTextNode(note));
+      ul.appendChild(li);
+    });
+  };
+  getNotes();
 });
